bc3e8e153acee9ac23f00ac11580d1d44fdf64bd
CynthiaG-W <cindydetec@gmail.com>
2026-08-30 16:01:04 +0300

    Fix initial database migration

--- backend/migrations/versions/2a6ec55b64cd_initial_database_baseline.py
+++ backend/migrations/versions/2a6ec55b64cd_initial_database_baseline.py
@@ -1,24 +1,33 @@
 """Initial database baseline
 
 Revision ID: 2a6ec55b64cd
-Revises: 
+Revises:
 Create Date: 2026-08-25 22:37:10.993804
-
 """
+
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2a6ec55b64cd'
+revision = "2a6ec55b64cd"
 down_revision = None
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        "services",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=150), nullable=False),
+        sa.Column("category", sa.String(length=50), nullable=False),
+        sa.Column("address", sa.String(length=255), nullable=True),
+        sa.Column("latitude", sa.Float(), nullable=True),
+        sa.Column("longitude", sa.Float(), nullable=True),
+        sa.PrimaryKeyConstraint("id")
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table("services")
